984fb2ad6be2ca281280d5b61c82b971f9969497
hetaodie <weixu6130@gmail.com>
2016-05-26 15:30:21 +0800

    添加gif的播放控件

---
+++ XGIFView/ViewController.m
@@ -0,0 +1,43 @@
+//
+//  ViewController.m
+//  XGIFView
+//
+//  Created by xlx on 16/3/31.
+//  Copyright © 2016年 xlx. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "XGIFView.h"
+
+
+#define DemoURL @"http://image.nihaowang.com/news/2015-04-27/c30f866d-9300-4f6e-86f6-58f408630e14.gif"
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet XGIFView *testView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    XGIFView *gifView = [[XGIFView alloc]initWithFrame:CGRectMake(0, 0, 200, 200)];
+    gifView.gifImageWithLocalName = @"gifDemo";
+//    gifView.gifNetWorkURL = DemoURL;
+    gifView.center = self.view.center;
+    [self.view addSubview:gifView];
+    
+    self.testView.gifNetWorkURL = DemoURL;
+    
+    
+}
+- (IBAction)clearCache:(id)sender {
+    [XGIFView clearCache];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
